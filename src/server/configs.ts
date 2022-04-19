d4598c59e89caac65be9a88db7e4da69b4ffb916
Nedzad Hafizovic <nedzad.hafizovic@softhouse.se>
2022-04-20 00:22:52 +0200

    db connection

--- src/server/configs.ts
+++ src/server/configs.ts
@@ -3,9 +3,9 @@ import {IConfigs} from "./domain/IConfigs";
 export const configs: IConfigs = {
     mongodb: {
         url: 'localhost',
-        port: 27017,
-        username: '',
-        password: '',
-        collection: 'test',
+        port: 3306,
+        username: 'megabooker-admin',
+        password: '8P_j4Hps:qB?',
+        collection: 'apartments',
     }
 }
